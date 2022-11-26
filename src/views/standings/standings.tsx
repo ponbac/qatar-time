37332b6720fe1e8bb270d21e48bc7f389dd77903
ponbac <root@ponbac.xyz>
2022-11-26 14:48:27 +0100

    oopsie

--- src/views/standings/standings.tsx
+++ src/views/standings/standings.tsx
@@ -6,13 +6,13 @@ import GroupStandings from "./group";
 const Standings = () => {
   return (
     <div className="min-h-screen font-novaMono flex flex-col items-center justify-center my-6">
-      <h1 className="text-4xl font-bold">Group Standings</h1>
       <motion.div
         className="flex flex-col items-center justify-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
       >
+        <h1 className="text-4xl font-bold">Group Standings</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 lg:gap-12 mt-8">
           <GroupStandings groupName="A" />
           <GroupStandings groupName="B" />
