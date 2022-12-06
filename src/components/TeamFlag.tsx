21ba024c7ca9ec70bef60e7e88922cf16563fd6b
Pontus Backman <root@ponbac.xyz>
2022-12-06 21:25:47 +0100

    fix

--- src/components/TeamFlag.tsx
+++ src/components/TeamFlag.tsx
@@ -8,23 +8,21 @@ type TeamFlagProps = {
 const TeamFlag = (props: TeamFlagProps) => {
   const { team, width, className = undefined } = props;
 
-  if (team === null) {
-    return <></>;
-  }
+  const flagPath = team
+    ? isUrl(team.flagCode)
+      ? team.flagCode
+      : team.flagCode.length < 7
+      ? `https://flagicons.lipis.dev/flags/4x3/${team.flagCode}.svg`
+      : `/images/team-logos/${team.flagCode}.svg`
+    : "aq";
 
   return (
     <img
       style={{ width: width }}
       draggable="false"
       className={className ? className : "rounded-sm max-h-full"}
-      alt={`${team.name} flag`}
-      src={
-        isUrl(team.flagCode)
-          ? team.flagCode
-          : team.flagCode.length < 7
-          ? `https://flagicons.lipis.dev/flags/4x3/${team.flagCode}.svg`
-          : `/images/team-logos/${team.flagCode}.svg`
-      }
+      alt={`${team ? team.name : "Unknown"} flag`}
+      src={flagPath}
     />
   );
 };
